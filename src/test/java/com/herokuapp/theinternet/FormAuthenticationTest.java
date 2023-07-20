dc7dbc2a276b6dafbd2747cb4892ef7425c1aa74
tomaszmellerbldr <122870352+tomaszmellerbldr@users.noreply.github.com>
2023-07-20 15:07:59 +0200

    - HomePage delete - Adding new test FormAuthenticationTest

---
+++ src/test/java/com/herokuapp/theinternet/FormAuthenticationTest.java
@@ -0,0 +1,18 @@
+package com.herokuapp.theinternet;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Test;
+import utilities.TestUtilities;
+
+public class FormAuthenticationTest extends TestUtilities {
+    private final Logger logger = LogManager.getLogger(FormAuthenticationTest.class);
+
+    @Test
+    public void formAuthenticationSteps() {
+        logger.info("Performing test steps...");
+
+
+        logger.info("Performing test steps...DONE!");
+    }
+}
